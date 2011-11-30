b066f94e319b0b212455901514f6e508573f0a1e
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-11-30 18:22:41 +0700

    v0.04

--- lib/Task/BeLike/SHARYANTO/Authored.pm
+++ lib/Task/BeLike/SHARYANTO/Authored.pm
@@ -24,6 +24,8 @@ Excluded are other Task::* modules, or modules already deletd from CPAN.
 
 =pkg App::OrgUtils
 
+=pkg App::SerializeUtils
+
 =pkg App::UniqFiles
 
 =pkg App::perlmv
@@ -148,208 +150,7 @@ Excluded are other Task::* modules, or modules already deletd from CPAN.
 
 =pkg Process::KillTree
 
-=pkg SHARYANTO::Utils
-
-=pkg Setup::File
-
-=pkg Setup::File::Symlink
-
-=pkg Setup::Multi
-
-=pkg Setup::Test
-
-=pkg Setup::Text::Snippet
-
-=pkg Setup::Unix::User
-
-=pkg Sub::Spec
-
-=pkg Sub::Spec::BashComplete
-
-=pkg Sub::Spec::Clause::drops_privilege
-
-=pkg Sub::Spec::CmdLine
-
-=pkg Sub::Spec::ConvertArgs::Array
-
-=pkg Sub::Spec::DepChecker
-
-=pkg Sub::Spec::Examples
-
-=pkg Sub::Spec::Exporter
-
-=pkg Sub::Spec::Gen::ForModule
-
-=pkg Sub::Spec::Gen::ReadTable
-
-=pkg Sub::Spec::GetArgs::Argv
-
-=pkg Sub::Spec::GetArgs::Array
-
-=pkg Sub::Spec::GetArgs::GetPost
-
-=pkg Sub::Spec::GetArgs::Hash
-
-=pkg Sub::Spec::HTTP
-
-=pkg Sub::Spec::HTTP::Server
-
-=pkg Sub::Spec::Object
-
-=pkg Sub::Spec::Runner
-
-=pkg Sub::Spec::To::HTML
-
-=pkg Sub::Spec::To::Org
-
-=pkg Sub::Spec::To::Pod
-
-=pkg Sub::Spec::To::Text
-
-=pkg Sub::Spec::URI
-
-=pkg Sub::Spec::URI::http
-
-=pkg Sub::Spec::Wrapper
-
-=pkg WWW::GudangAPI
-=pkg Acme::CPANAuthors::Female
-
-=pkg Acme::CPANAuthors::Indonesian
-
-=pkg Any::Renderer::Pretty::Console
-
-=pkg Any::Renderer::Pretty::HTML
-
-=pkg App::HTMLCalendarMonth::Indonesia
-
-=pkg App::OrgUtils
-
-=pkg App::SerializeUtils
-
-=pkg App::UniqFiles
-
-=pkg App::perlmv
-
-=pkg Array::Find
-
-=pkg Array::Functions::Undoable
-
-=pkg Array::Iterator
-
-=pkg Bench
-
-=pkg Business::ID::NIK
-
-=pkg Business::ID::NPWP
-
-=pkg Business::ID::SIM
-
-=pkg Calendar::Indonesia::Holiday
-
-=pkg Config::Ini::OnDrugs
-
-=pkg Data::Dump::Ala::SHARYANTO
-
-=pkg Data::Dump::OneLine
-
-=pkg Data::Dump::PHP
-
-=pkg Data::Dump::Partial
-
-=pkg Data::Dump::Ruby
-
-=pkg Data::Format::Pretty
-
-=pkg Data::Format::Pretty::Console
-
-=pkg Data::Format::Pretty::HTML
-
-=pkg Data::Format::Pretty::JSON
-
-=pkg Data::Format::Pretty::PHP
-
-=pkg Data::Format::Pretty::PHPSerialization
-
-=pkg Data::Format::Pretty::Perl
-
-=pkg Data::Format::Pretty::Ruby
-
-=pkg Data::Format::Pretty::YAML
-
-=pkg Data::ModeMerge
-
-=pkg Data::Sah::Util
-
-=pkg Devel::EndStats
-
-=pkg File::RsyBak
-
-=pkg File::Slurp::Shortcuts
-
-=pkg Finance::Bank::ID::BCA
-
-=pkg Finance::Bank::ID::Mandiri
-
-=pkg Gepok
-
-=pkg Git::Bunch
-
-=pkg HTTP::Daemon::UNIX
-
-=pkg LWP::Protocol::http::SocketUnixAlt
-
-=pkg Language::Expr
-
-=pkg Ledger::Parser
-
-=pkg Lingua::EN::Number::Format::MixWithWords
-
-=pkg Lingua::ID::Number::Format::MixWithWords
-
-=pkg Lingua::ID::Nums2Words
-
-=pkg Lingua::ID::Words2Nums
-
-=pkg Lingua::ZH::PinyinConvert::ID
-
-=pkg Locale::ID::GuessGender::FromFirstName
-
-=pkg Log::Any::Adapter::Callback
-
-=pkg Log::Any::Adapter::ScreenColoredLevel
-
-=pkg Log::Any::App
-
-=pkg Log::Dispatch::Dir
-
-=pkg Object::BlankStr
-
-=pkg Object::NulStr
-
-=pkg Object::SpaceBackStr
-
-=pkg Org::Parser
-
-=pkg Org::To::HTML
-
-=pkg Org::To::Pod
-
-=pkg Org::To::Text
-
-=pkg Parse::Netstat
-
-=pkg Parse::Number::EN
-
-=pkg Parse::Number::ID
-
-=pkg Parse::PhoneNumber::ID
-
-=pkg Passwd::Unix::Alt
-
-=pkg Pod::Weaver::Plugin::SubSpec
-
-=pkg Process::KillTree
+=pkg RSS::From::Forum::VBulletin
 
 =pkg RSS::From::Forum::vBulletin
 
@@ -414,6 +215,8 @@ Excluded are other Task::* modules, or modules already deletd from CPAN.
 =pkg Sub::Spec::URI
 
 =pkg Sub::Spec::URI::http
+
+=pkg Sub::Spec::Undo::Storage
 
 =pkg Sub::Spec::Use
 
